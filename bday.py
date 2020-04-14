7cc202d0b2409f4f1d4f3d1655f674d8a788d1a8
micy <micyeung@gmail.com>
2020-04-14 08:36:50 +0800

    modify str_to_datetime

--- bday.py
+++ bday.py
@@ -5,12 +5,10 @@ import datetime
 _FILE = 'bday.json'
 
 def str_to_datetime(sdate: str) -> datetime:
-    if sdate == '':
-        dt = datetime.datetime.today()
-    else:
-        dt = datetime.datetime.strptime(sdate, "%Y%m%d")
-    
+    dt = datetime.datetime.today() if sdate == '' else datetime.datetime.strptime(sdate, "%Y%m%d")
+
     return dt 
+
 
 def datetime_to_str(dt: datetime) -> str:
     return dt.strftime('%Y%m%d')
@@ -20,7 +18,7 @@ class FindBDay():
     def __init__(self, sdate: str):
         self.in_date = sdate
         self.file = _FILE
-    
+
     def is_bday(self, dt:datetime) -> bool:
         if dt.weekday() in (5, 6):
             return False
@@ -31,24 +29,22 @@ class FindBDay():
             if dt.strftime('%Y/%m/%d') in bday['Holiday']:
                 return False
 
-            return True
+        return True
 
     def get_bday(self, dt:datetime) -> datetime:
         bday = dt
         while True:
             if self.is_bday(bday):
                 break
-
             bday = bday - datetime.timedelta(days=1)
-        
         return bday
 
     def get_lday(self, dt:datetime) -> datetime:
         bday = self.get_bday(dt)
         bday = bday - datetime.timedelta(days=1)
         lday = self.get_bday(bday)
-
         return lday
+
 
 def main(sdate):
     print('sdate:{}'.format(sdate))
@@ -62,7 +58,6 @@ def main(sdate):
 
     print('the lastest lday is {}'.format(datetime_to_str(fbd.get_lday(dt))))
 
-    
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2 and len(sys.argv[1]) == 8 and sys.argv[1].isdigit():
@@ -70,6 +65,6 @@ if __name__ == '__main__':
     elif len(sys.argv) == 1:
         sdate = ''
     else:
-         exit('Usage: {} YYYYMMDD').format(sys.argv[0])
+        exit('Usage: {} YYYYMMDD').format(sys.argv[0])
 
     main(sdate)
